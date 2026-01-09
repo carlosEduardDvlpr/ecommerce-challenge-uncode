bee9f0c6a2a0d0f28f1d9f9a2731dfa69993d78d
TechnoGET <atendimento.technoget@gmail.com>
2026-01-09 01:06:53 -0300

    adjust style banner view

--- app/(public)/(home)/_components/banner/banner.view.tsx
+++ app/(public)/(home)/_components/banner/banner.view.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function BannerView() {
   return (
     <div className="p-pattern w-full text-white h-105 relative">
-      <div className="absolute z-2 mt-30">
+      <div className="absolute z-2 mt-20">
         <h1 className="text-4xl font-medium italic">UNCommerce</h1>
         <p className="text-xl">
           Qualidade tecnologica que{' '}
