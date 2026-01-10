a8cb6dcff286b2e39b0fe19039ffe67b5b03780c
carloseduardo.dvlper@gmail.com <atendimento.technoget@gmail.com>
2026-01-10 02:06:13 -0300

    style: adjust in height

--- app/(public)/(home)/_components/banner/banner.view.tsx
+++ app/(public)/(home)/_components/banner/banner.view.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export function BannerView() {
   return (
-    <div className="p-pattern w-full text-white h-85 relative">
-      <div className="absolute z-2 mt-20">
+    <div className="p-pattern w-full text-white h-95 relative">
+      <div className="absolute z-2 mt-30">
         <h1 className="text-4xl font-medium italic">UNCommerce</h1>
         <p className="text-xl">
           Qualidade tecnologica que{' '}
