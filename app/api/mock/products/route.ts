3b2c5d5148100c1984628f9357364971c061ced8
carloseduardo.dvlper@gmail.com <atendimento.technoget@gmail.com>
2026-01-09 22:57:08 -0300

    refactor(API): remove parameter "offer", replaced for new category "ofertas"

--- app/api/mock/products/route.ts
+++ app/api/mock/products/route.ts
@@ -4,7 +4,6 @@ import {
   filterProductByCategory,
   filterProductById,
   filterProductByName,
-  filterProductsOffers,
 } from '../utils/product-filters';
 
 export async function GET(req: NextRequest) {
@@ -13,12 +12,10 @@ export async function GET(req: NextRequest) {
   const id = searchParams.get('id');
   const category = searchParams.get('category');
   const name = searchParams.get('name');
-  const offer = searchParams.get('offer');
 
   if (id) return Response.json(filterProductById(Number(id)));
   if (category) return Response.json(filterProductByCategory(category));
   if (name) return Response.json(filterProductByName(name));
-  if (offer === 'true') return Response.json(filterProductsOffers());
 
   return Response.json(products);
 }
