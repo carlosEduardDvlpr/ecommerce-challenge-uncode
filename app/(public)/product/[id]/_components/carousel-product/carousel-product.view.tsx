643a0be260727a571f42916f02fc374ecafce493
carloseduardo.dvlper@gmail.com <atendimento.technoget@gmail.com>
2026-01-10 02:07:12 -0300

    feat(carousel-component): render carousel itens

---
+++ app/(public)/product/[id]/_components/carousel-product/carousel-product.view.tsx
@@ -0,0 +1,36 @@
+import { Product } from '@/app/api/mock/products';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import Image from 'next/image';
+
+export function CarouselProductView({ product }: { product: Product }) {
+  return (
+    <Carousel className="w-full">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <Card className="p-0">
+              <CardContent className="flex items-center justify-center p-6 relative aspect-5/4">
+                <Image
+                  className="object-cover"
+                  src={product.image}
+                  alt={product.name}
+                  fill
+                  unoptimized
+                />
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="bg-primary text-white hover:bg-primary" />
+      <CarouselNext className="bg-primary text-white hover:bg-primary" />
+    </Carousel>
+  );
+}
