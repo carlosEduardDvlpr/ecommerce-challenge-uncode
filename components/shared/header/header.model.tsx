8445070c0d137657a3af2e110e881f7066661a52
carloseduardo.dvlper@gmail.com <atendimento.technoget@gmail.com>
2026-01-11 12:29:43 -0300

    refactor(header): adjusting browser redirection in search results

--- components/shared/header/header.model.tsx
+++ components/shared/header/header.model.tsx
@@ -1,16 +1,21 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useState, type FormEvent } from 'react';
 
 export function useHeaderModel() {
   const [searchWord, setSearchWord] = useState('');
   const router = useRouter();
+  const pathname = usePathname();
 
   const handleSearch = (e: FormEvent) => {
     e.preventDefault();
     (document.activeElement as HTMLElement)?.blur();
 
+    if (pathname.includes('search')) {
+      router.replace(`/search/${searchWord}`);
+      return;
+    }
     router.push(`/search/${searchWord}`);
   };
 
