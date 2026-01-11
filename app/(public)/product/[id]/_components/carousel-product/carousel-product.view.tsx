b9e2f445f111cebc409e1b92c45844e137eb8aab
carloseduardo.dvlper@gmail.com <atendimento.technoget@gmail.com>
2026-01-11 08:30:57 -0300

    fix loader in svg image

--- app/(public)/product/[id]/_components/carousel-product/carousel-product.view.tsx
+++ app/(public)/product/[id]/_components/carousel-product/carousel-product.view.tsx
@@ -11,26 +11,29 @@ import Image from 'next/image';
 
 export function CarouselProductView({ product }: { product: Product }) {
   return (
-    <Carousel className="w-full">
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <Card className="p-0">
-              <CardContent className="flex items-center justify-center p-6 relative aspect-5/4">
-                <Image
-                  className="object-cover"
-                  src={product.image}
-                  alt={product.name}
-                  fill
-                  unoptimized
-                />
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="bg-primary text-white hover:bg-primary" />
-      <CarouselNext className="bg-primary text-white hover:bg-primary" />
-    </Carousel>
+    <div className="flex justify-center">
+      <Carousel className="md:w-full w-[80%]">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card className="p-0">
+                <CardContent className="flex items-center justify-center p-6 relative aspect-5/4">
+                  <Image
+                    className="object-cover"
+                    src={product.image}
+                    alt={product.name}
+                    fill
+                    unoptimized
+                    loading="eager"
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="bg-primary text-white hover:bg-primary" />
+        <CarouselNext className="bg-primary text-white hover:bg-primary" />
+      </Carousel>
+    </div>
   );
 }
