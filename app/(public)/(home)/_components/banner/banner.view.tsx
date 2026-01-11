9cc3ba57519c9a272c866bd8de99a9fba742dfe5
carloseduardo.dvlper@gmail.com <atendimento.technoget@gmail.com>
2026-01-11 08:27:05 -0300

    style(banner): adjust responsive and image loader

--- app/(public)/(home)/_components/banner/banner.view.tsx
+++ app/(public)/(home)/_components/banner/banner.view.tsx
@@ -2,16 +2,16 @@ import Image from 'next/image';
 
 export function BannerView() {
   return (
-    <div className="p-pattern w-full text-white h-95 relative">
-      <div className="absolute z-2 mt-30">
-        <h1 className="text-4xl font-medium italic">UNCommerce</h1>
-        <p className="text-xl">
+    <div className="p-pattern w-full text-white md:h-95 h-45 relative">
+      <div className="absolute z-2 md:mt-30 mt-12">
+        <h1 className="md:text-4xl text-3xl font-medium italic">UNCommerce</h1>
+        <p className="md:text-xl text-base">
           Qualidade tecnologica que{' '}
           <span className="text-primary font-medium">você merece!</span>
         </p>
       </div>
 
-      <Image src="/banner.jpg" alt="Banner" fill className="brightness-30" />
+      <Image src="/banner.jpg" loading='eager' alt="Banner" fill className="brightness-30" />
     </div>
   );
 }
